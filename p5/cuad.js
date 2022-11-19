3dbc57935ad91fd6d5d221aea091e4c213242295
Herbert Spencer <hspencer@ead.cl>
2022-11-19 18:59:55 -0300

    líneas más delgadas y de colores

--- p5/cuad.js
+++ p5/cuad.js
@@ -1,60 +1,76 @@
 /**
  *
- * primera prueba
+ * navv
  */
 
 
-let n = 99;
+let n = 222;
 let N;
 
 function setup() {
 let htmlElem = document.getElementById('p5');
 let w = htmlElem.offsetWidth;
-  let sketch = createCanvas(w, windowHeight-10);
+  let sketch = createCanvas(w, windowHeight-7);
   sketch.parent("p5");
   N = [];
   for(let i = 0; i < n; i++){
     N.push(new Note());
   }
+  strokeCap(SQUARE);
 }
 
 class Note{
     constructor(){
-        this.w = 11;
+        this.w = 3;
         this.from = random(0, height/2);
-        this.to = random(this.from + this.w, height/2);
+        this.to = random(this.from + this.w, this.from + height/4);
         this.h = this.to - this.from;
         this.x = floor(random(width/this.w))*this.w;
-        this.y = random(height - this.h) + height;
+        this.y = random(-this.h/2, height);
         this.step = random(.2, 1.2);
+        this.over = false;
+        this.color = color(random(255),random(255),random(255));
     }
 
     go(){
-        this.draw();
         this.move();
+        this.draw(); 
     }
 
     draw(){
+
         noStroke();
-        fill("#FFFC");
+        fill("#FFFA");
         rect(this.x, this.y, this.w, this.h);
+
+        if(this.over){
+          stroke(this.color);//stroke("#E4752D");
+          strokeWeight(this.w/2);
+          // line(this.x - this.w/4, this.y, this.x - this.w/4, this.y+this.h);
+          noFill();
+          rect(this.x, this.y, this.w, this.h);
+          strokeWeight(1);
+          line(this.x + this.w/2, this.y + this.w/2, this.x + this.w/2, this.y + this.h - this.w/2);
+        }
     }
 
     move(){
         this.y -= this.step;
+        if(mouseX > this.x && mouseX < this.x+this.w
+          && mouseY > this.y && mouseY < this.y+this.h){
+            this.over = true;
+          } else {
+            this.over = false;
+          }
+        if(this.y + this.h < 0){
+          this.y = height;
+        }
     }
 }
 
 function draw() {
   clear();
-  for(let i = 0; i < N.length; i++){
-    N[i].go();
-  }
-
-  for(let i = 0; i < N.length; i++){
-    if(N[i].y +  N[i].h < 0){
-      N.splice(i, 1);
-      N.push(new Note())
-    }
-  }
+ for(let n of N){
+  n.go();
+ }
 }
