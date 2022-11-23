286e93158518be3ae152801720b407a864e81207
Herbert Spencer <hspencer@ead.cl>
2022-11-23 14:12:08 -0300

    adj

--- p5/cuad.js
+++ p5/cuad.js
@@ -8,9 +8,11 @@ let n = 222;
 let globalWidth = 3;
 let N;
 let data;
-let notes; 
+let notes;
 
-function preload(){
+let content;
+
+function preload() {
   data = loadJSON("../data.json", gotData, 'json');
 }
 
@@ -22,16 +24,19 @@ function gotData(response) {
 
 
 function setup() {
-let htmlElem = document.getElementById('p5');
-let w = htmlElem.offsetWidth;
-  let sketch = createCanvas(w, windowHeight-7);
+  let htmlElem = document.getElementById('p5');
+  content = document.getElementById('content');
+  print("content = ");
+  console.log(content);
+  let w = htmlElem.offsetWidth;
+  let sketch = createCanvas(w, windowHeight - 7);
   sketch.parent("p5");
   regen();
 }
 
-function regen(){
+function regen() {
   N = [];
-  for(let i = 0; i < n; i++){
+  for (let i = 0; i < n; i++) {
     N.push(new Note());
   }
   strokeCap(SQUARE);
@@ -39,94 +44,104 @@ function regen(){
   buildNotes(data);
 }
 
-function buildNotes(data){
-  for(let key in data){
-   let num = round(random(N.length - 1));
-   let cant = data.length;
-   let th = round(random(cant - 1));
-   let n = N[num];
-   n.x = 0;
-   n.w = width;
-   n.h = globalWidth * 5;
-   n.color = color(0);
-   n.title = data[key].title;
-   n.date = data[key].date;
-   n.video = data[key].video;
-   n.text = data[key].text;
-   notes.push(n);
+function buildNotes(data) {
+  for (let key in data) {
+    let num = round(random(N.length - 1));
+    let cant = data.length;
+    let th = round(random(cant - 1));
+    let n = N[num];
+    n.x = (width / 10) * 1;
+    n.w = (width / 10) * 8;
+    n.h = 99;
+    n.colorOver = color(0, 140);
+    n.color = color(0, 30);
+    n.title = data[key].title;
+    n.date = data[key].date;
+    n.video = data[key].video;
+    n.text = data[key].text;
+    n.index = num;
+    notes.push(n);
   }
-  console.log({notes});
- }
+  console.log({
+    notes
+  });
+}
 
-class Note{
-    constructor(data){
-        this.w = globalWidth;
-        this.from = random(0, height/2);
-        this.to = random(this.from + this.w, this.from + height/4);
-        this.h = this.to - this.from;
-        this.x = floor(random(width/this.w))*this.w;
-        this.y = random(-this.h/2, height);
-        this.step = random(.2, 1.2);
-        this.over = false;
-        this.color = color(random(255),random(255),random(255));
-        this.title = "";
+class Note {
+  constructor(data) {
+    this.w = globalWidth;
+    this.from = random(0, height / 2);
+    this.to = random(this.from + this.w, this.from + height / 4);
+    this.h = this.to - this.from;
+    this.x = floor(random(width / this.w)) * this.w;
+    this.y = random(-this.h / 2, height);
+    this.step = random(.2, 1.2);
+    this.over = false;
+    this.color = "#FFFA";
+    this.colorOver = color(random(255), random(255), random(255));
+    this.title = "";
+  }
+
+  go() {
+    this.move();
+    this.draw();
+  }
+
+  draw() {
+    noStroke();
+    fill(this.color);
+
+    if (this.over) {
+      fill(this.colorOver);
+      rect(this.x, this.y, this.w, this.h);
+      strokeWeight(1);
+      line(this.x + this.w / 2, this.y + this.w / 2, this.x + this.w / 2, this.y + this.h - this.w / 2);
     }
 
-    go(){
-        this.move();
-        this.draw(); 
-    }
+    rect(this.x, this.y, this.w, this.h);
+  }
 
-    draw(){
-        noStroke();
-        fill("#FFFA");
-        
-        if(this.over){
-          stroke(this.color);
-          //strokeWeight(this.w/2);
-          // line(this.x - this.w/4, this.y, this.x - this.w/4, this.y+this.h);
-          fill(this.color);
-          rect(this.x, this.y, this.w, this.h);
-          strokeWeight(1);
-          line(this.x + this.w/2, this.y + this.w/2, this.x + this.w/2, this.y + this.h - this.w/2);
-        }
-
-        rect(this.x, this.y, this.w, this.h);
+  move() {
+    this.y -= this.step;
+    if (mouseX > this.x && mouseX < this.x + this.w &&
+      mouseY > this.y && mouseY < this.y + this.h) {
+      this.over = true;
+    } else {
+      this.over = false;
     }
-
-    move(){
-        this.y -= this.step;
-        if(mouseX > this.x && mouseX < this.x+this.w
-          && mouseY > this.y && mouseY < this.y+this.h){
-            this.over = true;
-          } else {
-            this.over = false;
-          }
-        if(this.y + this.h < 0){
-          this.y = height;
-        }
+    if (this.y + this.h < 0) {
+      this.y = height;
     }
+  }
 }
 
 function draw() {
   clear();
- for(let n of N){
-  n.go();
- }
-
- for(let n of notes){
-  if(n.over){
-    let fontSize = 15;
-    textFont("Futura PT");
-    textSize(fontSize);
-    fill(0);
-    text(n.title, n.x, n.y + n.h + fontSize, width/4, 200);
+  for (let n of N) {
+    n.go();
   }
- }
+
+  for (let n of notes) {
+    if (n.over) {
+      let fontSize = 19;
+      let textMargin = 13
+      textFont("Futura PT");
+      textSize(fontSize);
+      fill("#FFF");
+      text(n.title, n.x + textMargin, n.y + textMargin, width * .68, 200);
+    }
+  }
 }
 
-function mousePressed(){
-  background(0);
+function mousePressed() {
+  for (let n of notes) {
+    if (n.over) {
+      document.getElementById("content").html = "Whatever";
+      removeElements();
+      let newTitle = createElement('h1', n.title);
+      newTitle.parent('content');
+    }
+  }
 }
 
 function windowResized() {
