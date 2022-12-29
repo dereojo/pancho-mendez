78c3773e61d5b49783f1edd1c3dddc3e2e77fedc
Herbert Spencer <hspencer@ead.cl>
2022-12-29 10:39:54 -0300

    cambios mayores
    mejoras en:
    - selectores (checkboxes para filtrar)
    - layout (para imágenes y videos)
    - foto aleatoria de pancho

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# Francisco Méndez Labee
+## Francisco Méndez Labee
 
-Este sitio documenta y presenta la obra del pintor arquitecto **Francisco Méndez Labbe**. 
+Este sitio documenta y presenta la obra del pintor arquitecto **Francisco Méndez Labbe** y busca construir un espacio de colaboración en torno a la memoria de este artista.  
 
-
-Esta iniciativa busca construir un espacio de colaboración en torno a la memoria de este artista.  
-
-
-Todavía no hay material
+El fondo de archivo se encuentra representado por las partículas gráficas en ascención del lado derecho de la pantalla. Cada objeto está anotado de la siguiente manera:
 
 ### Estructura de datos
 
 
 - [Planilla del archivo](https://docs.google.com/spreadsheets/d/e/2PACX-1vQswJh4DoOWUujtJQctDbYMHnoTjYHE8Q_bHzGXW6fnglidAJdE3F0r2-E4UcpUV9Eakt67X8i99ROF/pubhtml) que se exporta a XLS
 - [Herramienta](https://beautifytools.com/excel-to-json-converter.php) para convertir XLS a JSON
+
+Si no conoce el manejo de estas herramientas tendrá que hacerlo manualmente en el archivo [data.json](data.json).
--- js/nav.js
+++ js/nav.js
@@ -13,14 +13,24 @@ let data; // objeto de datos
 let notes; // arreglo a partir de los datos
 
 
-let content; // div del contenido: lado izquierso
+let content, mediaDiv, textDiv; // div del contenido: lado izquierso
 let divP5; // div de esta nav: lado derecho
+let divMainotes;
+let selectorContent, creditsContent;
 
 let toggleARQ, togglePIN, toggleDIB, toggleVID, toggleFOT;
 let font;
 let colorPal;
+let panchoFotos = [
+  "/images/fml_fotos_perfil/01.png",
+  "/images/fml_fotos_perfil/02.png",
+  "/images/fml_fotos_perfil/03.png",
+  "/images/fml_fotos_perfil/04.png",
+  "/images/fml_fotos_perfil/05.png",
+];
 
-function getCol(img){
+
+function getCol(img) {
   return img.get(round(random(img.width)), round(random(img.height)));
 }
 
@@ -28,6 +38,8 @@ function preload() {
   data = loadJSON("data.json", gotData, 'json');
   font = loadFont("data/Jost-Medium.ttf");
   colorPal = loadImage("images/fml_paintings/fml_pt_018.jpg");
+  selectorContent = loadStrings('selector.txt');
+  creditsContent = loadStrings('credits.txt');
 }
 
 function gotData(response) {
@@ -35,76 +47,16 @@ function gotData(response) {
   notes = [];
 }
 
-function setup() {
-  if(hasSelector === true){
-    toggleARQ = createCheckbox('arquitecto', false);
-    togglePIN = createCheckbox('Pintor', false);
-    toggleDIB = createCheckbox('artista gráfico', false);
-    toggleVID = createCheckbox('profesor de arquitectura y diseño', false);
-    toggleFOT = createCheckbox('fundador del instituto de arquitectura de la PUCV y de la Ciudad Abierta', false);
-    toggleARQ.parent(document.getElementById('ARQ'));
-    togglePIN.parent(document.getElementById('PIN'));
-    toggleDIB.parent(document.getElementById('DIB'));
-    toggleVID.parent(document.getElementById('VID'));
-    toggleFOT.parent(document.getElementById('FOT'));
-    toggleARQ.changed(toggle);
-    togglePIN.changed(toggle);
-    toggleDIB.changed(toggle);
-    toggleVID.changed(toggle);
-    toggleFOT.changed(toggle);
-  }
-
-  regen();
-}
-
-function toggle(){
-  print("toggleARQ = "+toggleARQ.checked());
-  print("togglePIN = "+togglePIN.checked());
-  print("toggleDIB = "+toggleDIB.checked());
-  print("toggleVID = "+toggleVID.checked());
-  print("toggleFOT = "+toggleFOT.checked());
-
-  for(n in notes){
-    if(toogleARQ.checked() && n.cat === 'arquitectura'){
-      n.color = color("DeepSkyBlue");
-      n.colorOver = color("SkyBlue");
-    }else{
-      n.color = color(0);
-      n.colorOver = color("white");
-    }
-  }
-  /*
-  if(toggleARQ.value())
-    case "FOT":
-      n.color = color(0);
-      n.colorOver = color("red");
-      break;
-    case "PIN":
-      n.color = color("DeepPink");
-      n.colorOver = color("red");
-      break;
-    case "VID":
-      n.color = color("cyan");
-      n.colorOver = color("blue");
-      break;
-    case "DIB":
-      n.color = color("OrangeRed");
-      n.colorOver = color("red");
-      break;
-    case "ARQ":
-      n.color = color("DeepSkyBlue");
-      n.colorOver = color("SkyBlue");
-      break;
-  }*/
-}
-
 function regen() {
+  /*
   content = document.getElementById('content');
   divP5 = document.getElementById('p5');
-  let divMain = document.getElementById('main');
+  divMainotes = document.getElementById('main');
+  */
   let w = divP5.offsetWidth;
-  let h = divMain.offsetHeight;
+  let h = divMainotes.offsetHeight;
   let sketch = createCanvas(w, h);
+
   sketch.parent(divP5);
   sketch.style('position', 'absolute');
   sketch.style('z-index', '1000');
@@ -117,13 +69,72 @@ function regen() {
   buildNotes(data);
 }
 
+function setup() {
+  content = document.getElementById('pancho');
+  divP5 = document.getElementById('p5');
+  divMainotes = document.getElementById('main');
+  mediaDiv = document.getElementById('media');
+  textDiv = document.getElementById('text');
+  newPancho();
+  recreateMenu();
+  regen();
+}
+
+function newPancho(){
+  let elt = document.getElementById('pancho');
+  let panchoContent = "<h2>Francisco Méndez Labbé</h2><img src='"+ panchoFotos[floor(random(panchoFotos.length))] +"' class='pancho'>"
+  let pancho = createDiv(panchoContent);
+  pancho.parent(elt);
+}
+
+function toggle() {
+
+  print("##################################");
+  print("togglePIN = " + togglePIN.checked());
+  print("toggleARQ = " + toggleARQ.checked());
+  print("toggleDIB = " + toggleDIB.checked());
+  print("toggleVID = " + toggleVID.checked());
+  print("toggleFOT = " + toggleFOT.checked());
+  print("##################################");
+
+  for (let n of notes) {
+    print(n.title);
+    if (toggleARQ.checked() && n.cat === 'arquitectura') {
+      n.active = true;
+    } else if (!toggleARQ.checked() && n.cat === 'arquitectura') {
+      n.active = false;
+    }
+    if (togglePIN.checked() && n.cat === 'pintura') {
+      n.active = true;
+    } else if (!togglePIN.checked() && n.cat === 'pintura') {
+      n.active = false;
+    }
+
+    if (toggleDIB.checked() && n.cat === 'dibujo') {
+      n.active = true;
+    } else if (!toggleDIB.checked() && n.cat === 'dibujo') {
+      n.active = false;
+    }
+
+    if (toggleVID.checked() && n.cat === 'video') {
+      n.active = true;
+    } else if (!toggleVID.checked() && n.cat === 'video') {
+      n.active = false;
+    }
+
+    if (toggleFOT.checked() && n.cat === 'foto') {
+      n.active = true;
+    } else if (!toggleFOT.checked() && n.cat === 'foto') {
+      n.active = false;
+    }
+  }
+}
+
 function buildNotes(data) {
   for (let key in data) {
     let num = round(random(N.length - 1));
     let cant = data.length;
-    let th = round(random(cant - 1));
     let n = N[num];
-    n.w = globalWidth * 2;
     n.x = round(random(width / n.w) * n.w);
     n.h = random(30, 60);
     n.title = data[key].title;
@@ -136,36 +147,36 @@ function buildNotes(data) {
     n.hasData = true;
     n.step *= 0.333;
 
-    switch(n.cat){
-      case "foto":
-        n.color = color(0);
-        n.colorOver = color("red");
-        break;
+    switch (n.cat) {
+
       case "pintura":
-        n.color = color("DeepPink");
-        n.colorOver = color("red");
+        n.color = color("blue");
+        n.colorOver = color("darkBlue");
+        break;
+      case "arquitectura":
+        n.color = color("goldenRod");
+        n.colorOver = color("gold");
+        break;
+      case "dibujo":
+        n.color = color("SkyBlue");
+        n.colorOver = color("deepSkyBlue");
         break;
       case "video":
         n.color = color("cyan");
         n.colorOver = color("blue");
         break;
-      case "dibujo":
-        n.color = color("OrangeRed");
-        n.colorOver = color("red");
-        break;
-      case "arquitectura":
-        n.color = color("DeepSkyBlue");
-        n.colorOver = color("SkyBlue");
+      case "foto":
+        n.color = color(0);
+        n.colorOver = color("gray");
         break;
     }
     notes.push(n);
   }
-  console.log({notes});
 }
 
 class Note {
   constructor(data) {
-    this.w = globalWidth;
+    this.w = globalWidth * 2;
     this.from = random(0, height / 2);
     this.to = random(this.from + this.w, this.from + height / 4);
     this.h = this.to - this.from;
@@ -173,11 +184,12 @@ class Note {
     this.y = random(-this.h / 2, height);
     this.step = random(.1, .8);
     this.over = false;
-    this.color = "#FFFA";
+    this.color = "#FFF3";
     this.colorOver = this.color;
     this.title = "-";
     this.cat = "",
-    this.hasData = false;
+      this.hasData = false;
+    this.active = false;
   }
 
   go() {
@@ -187,14 +199,20 @@ class Note {
 
   draw() {
     noStroke();
-    fill(this.color);
+    if (this.active) {
+      fill(this.color);
+      this.w = globalWidth * 3;
+    } else {
+      fill("#FFF3");
+      this.w = globalWidth;
+    }
 
     if (this.over) {
       fill(this.colorOver);
       rect(this.x, this.y, this.w, this.h);
-      strokeWeight(1);
-      line(this.x + this.w / 2, this.y + this.w / 2, this.x + this.w / 2, this.y + this.h - this.w / 2);
     }
+
+    noStroke();
     rect(this.x, this.y, this.w, this.h);
   }
 
@@ -220,6 +238,7 @@ function draw() {
 
   for (let n of notes) {
     n.go();
+    // rollover
     if (n.over) {
       let fontSize = 18;
       let textMargin = 15
@@ -233,7 +252,7 @@ function draw() {
       } else {
         textAlign(RIGHT);
         text(n.title, 0, n.y + fontSize, n.x - textMargin, 100);
-      }  
+      }
     }
   }
 }
@@ -241,25 +260,26 @@ function draw() {
 function mousePressed() {
   for (let n of notes) {
     if (n.over) {
-      console.log({n});
-      removeElements();
+
       clearContent();
 
-      let newTitle = createElement('h2', n.date);
-      newTitle.parent('content');
+      let newTitle = createElement('h5', n.date);
+      newTitle.parent(textDiv);
       let newText = createElement('p', n.text);
-      newText.parent('content');
+      newText.parent(textDiv);
 
       if (n.video) {
-        let videoElem = createDiv("<iframe class='vid-overlay' src=" + n.video + " width='100%' height='360' frameborder='0' allow='autoplay; fullscreen; picture-in-picture' allowfullscreen></iframe>");
-        videoElem.parent(divP5);
+        let videoElem = createDiv("<iframe class='vid-overlay' src=" + n.video + " width='100%' height='420' frameborder='0' allow='autoplay; fullscreen; picture-in-picture' allowfullscreen></iframe>");
+        videoElem.parent(mediaDiv);
       }
 
       if (n.image) {
         print("img = " + n.img);
-        let imageElem = createDiv("<div class='img-overlay'><img src="+n.image+" title="+n.title+" /></div>");
-        imageElem.parent(divP5);
+        let imageElem = createDiv("<div class='img-overlay'><img src=" + n.image + " title=" + n.title + " /></div>");
+        imageElem.parent(mediaDiv);
       }
+
+      // recreateMenu();
 
       let closeBtn = createButton("<span>cerrar</span>");
       closeBtn.class('close');
@@ -274,10 +294,55 @@ function windowResized() {
   regen();
 }
 
+function recreateMenu() {
+  //removeElements();
+  if (hasSelector === true) {
+    let elt = document.getElementById('selector');
+    elt.innerHTML = selectorContent + creditsContent;
+
+    let PIN = false;
+    let ARQ = false;
+    let DIB = false;
+    let FOT = false;
+    let VID = false;
+
+    for (let n of notes) {
+      if (n.active && n.cat === "pintura") {
+        PIN = true;
+      } else if (n.active && n.cat === "arquitectura") {
+        ARQ = true;
+      } else if (n.active && n.cat === "dibujo") {
+        DIB = true;
+      } else if (n.active && n.cat === "video") {
+        VID = true
+      } else if (n.active && n.cat === "foto") {
+        FOT = true;
+      }
+    }
+
+
+    toggleARQ = createCheckbox('arquitecto', ARQ);
+    togglePIN = createCheckbox('pintor', PIN);
+    toggleDIB = createCheckbox('artista gráfico', DIB);
+    toggleVID = createCheckbox('profesor de arquitectura y diseño', VID);
+    toggleFOT = createCheckbox('fundador del instituto de arquitectura de la PUCV y de la Ciudad Abierta', FOT);
+
+    toggleARQ.parent(document.getElementById('ARQ'));
+    togglePIN.parent(document.getElementById('PIN'));
+    toggleDIB.parent(document.getElementById('DIB'));
+    toggleVID.parent(document.getElementById('VID'));
+    toggleFOT.parent(document.getElementById('FOT'));
+
+    toggleARQ.changed(toggle);
+    togglePIN.changed(toggle);
+    toggleDIB.changed(toggle);
+    toggleVID.changed(toggle);
+    toggleFOT.changed(toggle);
+  }
+}
+
 function clearContent() {
-  console.log({
-    content
-  })
-  content.innerHTML = "";
   removeElements();
+  mediaDiv.innerHTML = " ";
+  recreateMenu();
 }
